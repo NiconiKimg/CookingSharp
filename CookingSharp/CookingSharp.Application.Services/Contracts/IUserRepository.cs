9c001fcedc9ac128a5298b694c59f507b88b1b75
Luhm4nn <emyluhmann@gmail.com>
2025-08-01 17:13:44 -0300

    Comienzo del CRUD User basndomé en Category. Se creó los archivos pertenecientes a Aplication y Domain

---
+++ CookingSharp/CookingSharp.Application.Services/Contracts/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookingSharp.Domain.Model;
+
+namespace CookingSharp.Application.Services.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(int id);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task User(Category user);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsWithEmailAsync(string mail, int? excludeId = null);
+    }
+}
