0fd1798a1814f89f85e2a0970b6548ebd5c918d4
Luhm4nn <emyluhmann@gmail.com>
2025-08-03 12:32:46 -0300

    Ajuste menor en la interfaz

--- CookingSharp/CookingSharp.Application.Services/Contracts/IUserRepository.cs
+++ CookingSharp/CookingSharp.Application.Services/Contracts/IUserRepository.cs
@@ -13,7 +13,6 @@ namespace CookingSharp.Application.Services.Contracts
         Task<IEnumerable<User>> GetAllAsync();
         Task<User> AddAsync(User user);
         Task UpdateAsync(User user);
-        Task User(Category user);
         Task<bool> DeleteAsync(int id);
         Task<bool> ExistsWithEmailAsync(string mail, int? excludeId = null);
     }
