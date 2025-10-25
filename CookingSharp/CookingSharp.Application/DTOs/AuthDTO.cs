37d82d3a1f41f222bb34e68cae8ef25b7cbf8a8e
Luhm4nn <emyluhmann@gmail.com>
2025-10-25 19:04:35 -0300

    feat: new Auth DTOs

--- CookingSharp/CookingSharp.Application/DTOs/AuthDTO.cs
+++ CookingSharp/CookingSharp.Application/DTOs/AuthDTO.cs
@@ -14,5 +14,23 @@ public class UserLoginDTO
 /// </summary>
 public class LoginResponseDTO
 {
-    public string Token { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public int UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
+
+public class LoginRequestDTO
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+public class RegisterRequestDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
