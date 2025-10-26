95af787eb5b5bc7054c4ffbff2774d473b3bd298
LucaTvl <ltrincavell@frro.utn.edu.ar>
2025-10-26 13:02:23 -0300

    feat: Create recipe form in web app

---
+++ CookingSharp/WebApp/DTOs/CategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.DTOs
+{
+    public class CategoryResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- CookingSharp/WebApp/DTOs/RecipeDTO.cs
+++ CookingSharp/WebApp/DTOs/RecipeDTO.cs
@@ -7,8 +7,14 @@ public class RecipeResponseDTO
     public string AuthorName { get; set; } = string.Empty;
     public ICollection<CategoryResponseDTO> Categories { get; set; } = new List<CategoryResponseDTO>();
 }
-public class CategoryResponseDTO
+public class RecipeCreateDTO
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ICollection<RecipeStepCreateDTO> Steps { get; set; } = new List<RecipeStepCreateDTO>();
+    public ICollection<int> CategoryIds { get; set; } = new List<int>();
+}
+public class RecipeStepCreateDTO
+{
+    public string Instruction { get; set; }
 }
