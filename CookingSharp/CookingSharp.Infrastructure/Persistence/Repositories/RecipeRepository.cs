b9f9e77c457b8f467c17927238d868333aab2480
NiconiKimg <pedemontenicolas2004@gmail.com>
2025-10-26 22:30:04 -0300

    fix: populate steps in recipes

--- CookingSharp/CookingSharp.Infrastructure/Persistence/Repositories/RecipeRepository.cs
+++ CookingSharp/CookingSharp.Infrastructure/Persistence/Repositories/RecipeRepository.cs
@@ -18,6 +18,7 @@ public class RecipeRepository : GenericRepository<Recipe>, IRecipeRepository
             .Include(r => r.User)
             .Include(r => r.Categories)
             .Include(r => r.Ratings)
+            .Include(r => r.Steps)
             .AsNoTracking()
             .ToListAsync();
     }
